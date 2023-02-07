33d182be25c32be4971511b5cc88cdf48d53c2bb
isiakpona-chuks <providingperspectivesent@gmail.com>
2023-02-07 18:02:04 +0100

    final-work

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
+const swaggerUiExpress = require('swagger-ui-express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swaggerDesign.json'); // don't want to override this with npm run swagger
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
 
 module.exports = router;
